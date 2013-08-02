f191613924b067c409cfa6355e487ae3d9cb15c0
Stan <Stan@InterLogic>
2013-08-02 21:23:31 +0300

    Changed minimal Table data space size that triggers output to console. Changed value from 128MB to 256MB.

--- Code/Program.cs
+++ Code/Program.cs
@@ -421,7 +421,7 @@ namespace DbScripter
 									Console.WriteLine("  Table \"" + DbObj.Name + "\" Script.Count = " + Script.Count + ".");
 								}*/
 
-								if (DbObj.DataSpaceUsed > 128 * 1000)
+								if (DbObj.DataSpaceUsed > 256 * 1000)
 								{
 									Console.WriteLine("Table \"" + DbObj.Name + "\" data size = " + DbObj.DataSpaceUsed / 1000 + " MB.");
 								}

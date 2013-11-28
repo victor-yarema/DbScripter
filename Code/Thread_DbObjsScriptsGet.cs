eec8fac4f5fcf7356616e067c2793ef226edeaff
Stan <Stan@InterLogic>
2013-11-28 17:38:54 +0200

    Implemented infinite retries when script get method throws exception with type "FailedOperationException".

--- Code/Thread_DbObjsScriptsGet.cs
+++ Code/Thread_DbObjsScriptsGet.cs
@@ -29,84 +29,95 @@ namespace DbScripter
 
 				for (int i = Parameter.DbObjs_BeginIndex; i < Parameter.DbObjs_EndIndex; i++)
 				{
-					switch (Parameter.DbObjs.Type)
+					bool TryToScript = true;
+					while (TryToScript)
 					{
-						case DbObjType.Default:
+						try
+						{
+							switch (Parameter.DbObjs.Type)
 							{
-								Default DbObj = (Default)Parameter.Db.Defaults[Parameter.DbObjs.Items[i].Name];
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						case DbObjType.Uddt:
-							{
-								UserDefinedDataType DbObj = (UserDefinedDataType)Parameter.Db.UserDefinedDataTypes[Parameter.DbObjs.Items[i].Name];
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						case DbObjType.Udtt:
-							{
-								UserDefinedTableType DbObj = (UserDefinedTableType)Parameter.Db.UserDefinedTableTypes[Parameter.DbObjs.Items[i].Name];
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						//
-						case DbObjType.Table:
-							{
-								Table DbObj = (Table)Parameter.Db.Tables[Parameter.DbObjs.Items[i].Name];
-								if (DbObj == null || DbObj.IsSystemObject)
-								{
-									//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
-									Parameter.DbObjs.Items[i].IsSystem = true;
-									break;
-								}
-								Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+								case DbObjType.Default:
+									{
+										Default DbObj = (Default)Parameter.Db.Defaults[Parameter.DbObjs.Items[i].Name];
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								case DbObjType.Uddt:
+									{
+										UserDefinedDataType DbObj = (UserDefinedDataType)Parameter.Db.UserDefinedDataTypes[Parameter.DbObjs.Items[i].Name];
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								case DbObjType.Udtt:
+									{
+										UserDefinedTableType DbObj = (UserDefinedTableType)Parameter.Db.UserDefinedTableTypes[Parameter.DbObjs.Items[i].Name];
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								//
+								case DbObjType.Table:
+									{
+										Table DbObj = (Table)Parameter.Db.Tables[Parameter.DbObjs.Items[i].Name];
+										if (DbObj == null || DbObj.IsSystemObject)
+										{
+											//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
+											Parameter.DbObjs.Items[i].IsSystem = true;
+											break;
+										}
+										Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
 
-								Parameter.DbObjs.Items[i].DataSpaceUsed = DbObj.DataSpaceUsed;
-								Parameter.DbObjs.Items[i].IndexSpaceUsed = DbObj.IndexSpaceUsed;
-							} break;
-						case DbObjType.View:
-							{
-								View DbObj = (View)Parameter.Db.Views[Parameter.DbObjs.Items[i].Name];
-								if (DbObj == null || DbObj.IsSystemObject)
-								{
-									//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
-									Parameter.DbObjs.Items[i].IsSystem = true;
-									break;
-								}
-								Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						//
-						case DbObjType.Udf:
-							{
-								UserDefinedFunction DbObj = (UserDefinedFunction)Parameter.Db.UserDefinedFunctions[Parameter.DbObjs.Items[i].Name];
-								if (DbObj == null || DbObj.IsSystemObject)
-								{
-									//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
-									Parameter.DbObjs.Items[i].IsSystem = true;
-									break;
-								}
-								Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						case DbObjType.Sp:
-							{
-								StoredProcedure DbObj = (StoredProcedure)Parameter.Db.StoredProcedures[Parameter.DbObjs.Items[i].Name];
-								if (DbObj == null || DbObj.IsSystemObject)
-								{
-									//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
-									Parameter.DbObjs.Items[i].IsSystem = true;
-									break;
-								}
-								Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						//
-						case DbObjType.Ftc:
-							{
-								FullTextCatalog DbObj = (FullTextCatalog)Parameter.Db.FullTextCatalogs[Parameter.DbObjs.Items[i].Name];
-								Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
-							} break;
-						default:
-							throw new Exception("Unknown Type = \"" + Parameter.DbObjs.Type.ToString() + "\".");
+										Parameter.DbObjs.Items[i].DataSpaceUsed = DbObj.DataSpaceUsed;
+										Parameter.DbObjs.Items[i].IndexSpaceUsed = DbObj.IndexSpaceUsed;
+									} break;
+								case DbObjType.View:
+									{
+										View DbObj = (View)Parameter.Db.Views[Parameter.DbObjs.Items[i].Name];
+										if (DbObj == null || DbObj.IsSystemObject)
+										{
+											//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
+											Parameter.DbObjs.Items[i].IsSystem = true;
+											break;
+										}
+										Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								//
+								case DbObjType.Udf:
+									{
+										UserDefinedFunction DbObj = (UserDefinedFunction)Parameter.Db.UserDefinedFunctions[Parameter.DbObjs.Items[i].Name];
+										if (DbObj == null || DbObj.IsSystemObject)
+										{
+											//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
+											Parameter.DbObjs.Items[i].IsSystem = true;
+											break;
+										}
+										Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								case DbObjType.Sp:
+									{
+										StoredProcedure DbObj = (StoredProcedure)Parameter.Db.StoredProcedures[Parameter.DbObjs.Items[i].Name];
+										if (DbObj == null || DbObj.IsSystemObject)
+										{
+											//throw new Exception("Can't find DbObj with name \"" + Parameter.DbObjs.Items[i].Name + "\".");
+											Parameter.DbObjs.Items[i].IsSystem = true;
+											break;
+										}
+										Parameter.DbObjs.Items[i].IsSystem = DbObj.IsSystemObject;
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								//
+								case DbObjType.Ftc:
+									{
+										FullTextCatalog DbObj = (FullTextCatalog)Parameter.Db.FullTextCatalogs[Parameter.DbObjs.Items[i].Name];
+										Parameter.DbObjs.Items[i].Script = DbObj.Script(so);
+									} break;
+								default:
+									throw new Exception("Unknown Type = \"" + Parameter.DbObjs.Type.ToString() + "\".");
+							}
+							TryToScript = false;
+						}
+						catch (FailedOperationException)
+						{
+						}
 					}
 				}
 			}
